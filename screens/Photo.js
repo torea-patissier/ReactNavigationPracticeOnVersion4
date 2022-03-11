dc79404440471927730df6b06b577abd0d4f4011
Toréa Patissier <torea.patissier@laplateforme.io>
2022-03-11 22:39:36 +0100

    Goback ok

--- screens/Photo.js
+++ screens/Photo.js
@@ -1,10 +1,23 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Button } from 'react-native'
 import React from 'react'
 
-const Photo = () => {
+const Photo = (props) => {
+
+  console.log(props);
+  // Le props récupéré vient de App.js qui hérite de HomeStackNav.js
+
+  const RetourEnArriere = () => {  
+    props.navigation.goBack();
+  }
+  // Bouton pour revenir en arrière
+
   return (
     <View style={styles.container}>
       <Text>Photo</Text>
+      <Button
+      title='Retour en arrière'
+      onPress={RetourEnArriere}
+      />
     </View>
   )
 }
